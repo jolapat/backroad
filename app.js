99c39d3ccd0dc34af145def99012fbd5625adb97
jolapat <jolapatience@gmail.com>
2023-08-20 15:57:48 +0100

    initial commit of backroad

---
+++ app.js
@@ -0,0 +1,25 @@
+let toggleBtn = document.querySelector(".toggle-btn");
+let mobileMenu = document.querySelector(".mobile-menu");
+let barMark = document.querySelector(".fa-bars");
+let closeMark = document.querySelector(".fa-xmark");
+
+closeMark.style.display = "none";
+let open = false;
+
+function toggleMenu() {
+ if (open == false) {
+  closeMark.style.display = "inline-block";
+  barMark.style.display = "none";
+  mobileMenu.style.top = "10vh";
+  open = true; 
+ } else{
+  closeMark.style.display = "none";
+  barMark.style.display = "inline-block";
+  mobileMenu.style.top = "-200vh";
+  open = false; 
+ }
+}
+
+toggleBtn.addEventListener("click", toggleMenu)
+
+
